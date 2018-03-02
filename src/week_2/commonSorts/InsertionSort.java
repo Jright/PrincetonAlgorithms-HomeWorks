4a7f6f3ec1267a3d61ac07285a179d9eba82f410
minchao.zhu <readthinkcommunicate@gmail.com>
2018-03-02 16:01:37 +0800

    week_2 insertion sort

--- src/week_2/commonSorts/InsertionSort.java
+++ src/week_2/commonSorts/InsertionSort.java
@@ -1,26 +1,26 @@
 package week_2.commonSorts;
 
-public class SelectionSort {
+
+public class InsertionSort {
 
     public static void main(String[] args){
         Integer[] array = new Integer[]{5,2,7,3,9,1,4,6,8};
-        selectionSort(array);
+        insertionSort(array);
         for(Integer i : array){
             System.out.print(i + " ");
         }
     }
 
-    public static void selectionSort(Integer[] array){
-        int N = array.length;
-
-        for(int i = 0; i < N ; i++){
-            int min = i;
-            for(int j = i + 1; j < N; j++){
-                if(less(array[j],array[min])){
-                    min = j;
+    private static void insertionSort(Integer[] array){
+        for(int i = 0; i < array.length; i++){
+            int j = i;
+            for(; j > 0;j--){
+                if(less(array[j],array[j - 1])){
+                    exchange(array,j,j - 1);
+                }else{
+                    break;
                 }
             }
-            exchange(array,i,min);
         }
     }
 
