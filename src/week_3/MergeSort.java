bb6d9a9fa9cc829b23f33e186b1ad5e62a0b81dc
minchao.zhu <readthinkcommunicate@gmail.com>
2018-03-07 13:31:37 +0800

    week_3 MergeSort's up-bottom bug fix(now clear of bug)

--- src/week_3/MergeSort.java
+++ src/week_3/MergeSort.java
@@ -1,18 +1,14 @@
 package week_3;
 
-
-import java.util.Arrays;
-
 /**
  * Created by jright on 2017/6/28.
  */
 public class MergeSort {
 
-
+    private static int mergeCount = 0;
     public static void main(String[] args) {
-        Comparable[] array = {1,3,5,7,9,2,4,6,8,10};
-        Comparable[] aux = new Comparable[array.length];
-        Arrays.fill(aux,0);
+        Comparable[] array = {4,2,1,3,5,8,6,9,7,0};
+        Comparable[] aux = array.clone();
         sort(array,aux,0,array.length - 1);
         for(Comparable i : aux){
             System.out.print(i + " ");
@@ -28,25 +24,36 @@ public class MergeSort {
      * @param high
      */
     public static void merge(Comparable[] a, Comparable[] result, int low, int mid, int high) {
+        mergeCount++;
+        System.out.println("Merge: low = " + low + ",mid = " + mid + ",high = " + high + ",MergeCount: " + mergeCount);
 
-        assert isSorted(a); //Comparable[] a : a[lo....mid] is sorted, and a[mid + 1, high] is sorted as well.
-        assert isSorted(result); //Comparable[] result, we save the result array to it.
-
-        //now we merge the two sorted subarrays a and result
+        //now we merge the two sorted subarrays of array a
         int i = low, j = mid + 1;//Two pointers point to the start of two sorted subarrays respectively
         int k = low;
 
         while (i <= mid && j <= high) {
             int compare = a[i].compareTo(a[j]);
-            result[k++] = compare > 0 ? a[j++] : a[i++];
+            result[k] = compare > 0 ? a[j++] : a[i++];
+            k++;
         }
+        printArray(1,result);
 
         while(i <= mid){
-            result[k++] = a[i++];
+            result[k] = a[i++];
+            k++;
         }
 
+        printArray(2,result);
+
         while(j <= high){
-            result[k++] = a[j++];
+            result[k] = a[j++];
+//            System.out.println("result[k] = " + result[k]);
+            k++;
+        }
+
+        printArray(3,result);
+        for(int q = 0; q < result.length; q++){
+            a[q] = result[q];
         }
     }
 
@@ -57,6 +64,13 @@ public class MergeSort {
         int mid = low + (high - low) / 2;
         sort(a,result,low,mid); //sort the left half of array a, will return directly when there is only one element of the array.
         sort(a,result,mid + 1,high);
+
+        //In this step, we already know that the left half and the right half of array is sorted. And it is possible that this array is already sorted
+        //So we add one check to see if it is already sorted. In this example, if we add this judgement, the total run times of merge has reduced from 9 to 7
+        if(a[mid].compareTo(a[mid + 1]) < 0){
+            System.out.println("Sort 3, no need to merge, already sorted, a[mid] = " + a[mid] + ",a[mid + 1] = " + a[mid + 1]);
+            return;
+        }
         merge(a,result,low,mid,high);//if the program executed to this line, we merge a array with two elements first, then return to the last sort() call
     }
 
@@ -73,6 +87,13 @@ public class MergeSort {
         return true;
     }
 
+    private static void printArray(int count,Comparable[] array){
+        System.out.print(count + ": ");
+        for(Comparable c1 : array){
+            System.out.print(c1 + " ");
+        }
+        System.out.println();
+    }
     //递归实现归并排序
 //    static void merge_sort_recursive(int[] arr, int[] result, int start, int end) {
 //        if (start >= end)//如果目前的数组只有一个元素则返回
